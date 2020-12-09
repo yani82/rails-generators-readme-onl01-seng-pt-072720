84f508c71519971587d0a02d6cea81d5ee386983
yani82 <yani.iman1@gmail.com>
2020-12-08 20:38:34 -0500

    commit tree 9c93749f7ed19855ce0ba912bd5c8563aecdc32a

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20201208175438) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string   "post_status"
   end
 
 end
